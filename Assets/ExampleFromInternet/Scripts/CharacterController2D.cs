ff545ba59ae5609ef725804b5734bd38d1dd6679
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-10-10 10:32:54 +0200

    Add PlayerController from internet

---
+++ Assets/ExampleFromInternet/Scripts/CharacterController2D.cs
@@ -0,0 +1,35 @@
+/*
+ * Source code: https://www.youtube.com/watch?v=Bf_5qIt9Gr8
+ */
+
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace ExampleFromInternet.Scripts
+{
+    public class CharacterController2D : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 30f;
+        
+        private Rigidbody2D _rigidbody2D;
+        private Vector3 _moveDir;
+
+        private void Awake() => _rigidbody2D = GetComponent<Rigidbody2D>();
+
+        private void Update()
+        {
+            float moveX = 0f;
+            float moveY = 0f;
+
+            if (Input.GetKey(KeyCode.W)) moveY = 1f;
+            if (Input.GetKey(KeyCode.S)) moveY = -1f;
+            if (Input.GetKey(KeyCode.A)) moveX = -1f;
+            if (Input.GetKey(KeyCode.D)) moveX = 1f;
+
+            _moveDir = new Vector3(moveX, moveY).normalized;
+        }
+
+        private void FixedUpdate() => _rigidbody2D.velocity = _moveDir * moveSpeed;
+
+    }
+}
